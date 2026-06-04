3faf8db00f003106d65bd0b51fcec36ced12748e
palakbodkhe <palakbodkhe2617@gmail.com>
2026-06-04 17:51:06 +0530

    Priority Queue obj implementation

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -5,7 +5,7 @@ public class Solution{
       int complement = target - nums[i];
 
       if(map.containsKey(complement)){
-        return new int[] {map.get(complement), i);
+        return new int[] {map.get(complement), i};
       }
        map.put(nums[i], i);
     }
